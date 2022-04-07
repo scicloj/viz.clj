972cfff402f99b0edbce3fad809d397ef0b9c592
daslu <daniel.slutsky@gmail.com>
2022-04-08 02:42:18 +0300

    moved to a build-clj setup

---
+++ build.clj
@@ -0,0 +1,27 @@
+(ns build (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
+                    [org.corfield.build :as bb]))
+
+(def lib 'org.scicloj/viz.clj)
+(def version "1-alpha1")
+#_ ; alternatively, use MAJOR.MINOR.COMMITS:
+(def version (format "1.0.%s" (b/git-count-revs nil)))
+
+(defn test "Run the tests." [opts]
+  (bb/run-tests opts))
+
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/run-tests)
+      (bb/clean)
+      (bb/jar)))
+
+(defn install "Install the JAR locally." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/install)))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/deploy)))
