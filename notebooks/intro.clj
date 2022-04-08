e2bf27d2d0f553076c10a74e336e4b9b9131e144
daslu <daniel.slutsky@gmail.com>
2022-04-08 10:10:19 +0300

    updated tutorial

--- notebooks/intro.clj
+++ notebooks/intro.clj
@@ -69,6 +69,128 @@
 
 ;; Throughout this tutorial, we will gradually discover how they work.
 
+;; ## How does it work?
+
+;; Throughout the pipeline of transformations, Viz.clj progresses a Clojure map containing the necessary details for plotting.
+
+;; Most of those details are [template substitutions](https://github.com/jsa-aerial/hanami#templates-substitution-keys-and-transformations) of [Hanami](https://github.com/jsa-aerial/hanami), but the beginner user does not need to know that.
+
+;; (As we'll see below, datasets are kept under a special key, complying with the [Metamorph](https://github.com/scicloj/metamorph) approach, but the beginner user does not need to know that.)
+
+;; The plot type is also kept under a special key.
+
+;; The final `viz/viz` call combines all the details and applies the Hanami substitutions, to get a Vega-Lite plot.
+
+(-> [{:x 1 :y 2}
+     {:x 2 :y 4}
+     {:x 3 :y 9}]
+    viz/data)
+
+(-> [{:x 1 :y 2}
+     {:x 2 :y 4}
+     {:x 3 :y 9}]
+    viz/data
+    (viz/type :point))
+
+(-> [{:x 1 :y 2}
+     {:x 2 :y 4}
+     {:x 3 :y 9}]
+    viz/data
+    (viz/type :point)
+    (viz/mark-size 200))
+
+;; The final result is marked to have the Vega kind, and just renders accordingly.
+
+(-> [{:x 1 :y 2}
+     {:x 2 :y 4}
+     {:x 3 :y 9}]
+    viz/data
+    (viz/type :point)
+    (viz/mark-size 200)
+    (viz/color :x)
+    viz/viz)
+
+;; Let us render it as a plain Clojure data structure, using the naive kind:
+
+(-> [{:x 1 :y 2}
+     {:x 2 :y 4}
+     {:x 3 :y 9}]
+    viz/data
+    (viz/type :point)
+    (viz/mark-size 200)
+    (viz/color :x)
+    viz/viz
+    kind/naive)
+
+;; ## Just using Hanami
+
+;; Just like using Hanami allows one to use the full flexibility of Vega-Lite (but the beginner user may not need to worry about it too much), using Viz.clj allows one to use the full flexibility of Hanami (but the beginner user does not need to worry about it too much).
+
+;; Fot those who care about it, Viz.clj can be seen as a way to generate and apply Hanami Template substitutions in a dataset-friendly way (see examples with datasets below).
+
+;; (Thus, it also provides a dydactic way of getting many useful examples of Hanami substitions.)
+
+;; For example, let us see the Hanami substitutions comprising the above plot:
+
+(-> [{:x 1 :y 2}
+     {:x 2 :y 4}
+     {:x 3 :y 9}]
+    viz/data
+    (viz/type :point)
+    (viz/mark-size 200)
+    (viz/color :x))
+
+;; We just learned about the `:COLOR` and `:SIZE` keys.
+
+;; It is possible to just pass them directly as substitutions at the final `viz/viz` call:
+
+(-> [{:x 1 :y 2}
+     {:x 2 :y 4}
+     {:x 3 :y 9}]
+    viz/data
+    (viz/type :point)
+    (viz/viz {:COLOR "x"
+              :SIZE 200}))
+
+;; To make it even closer to a Hanami call, we can use the corresponding Hanami template instead of the shorthand `:point`:
+
+(-> [{:x 1 :y 2}
+     {:x 2 :y 4}
+     {:x 3 :y 9}]
+    viz/data
+    (viz/type ht/point-chart)
+    (viz/viz {:COLOR "x"
+              :SIZE 200}))
+
+;; In the case of plain data (e.g., vector of maps rather than a dataset), that could be also written this way, passing the data as a substitution as well:
+
+(-> {}
+    (viz/type ht/point-chart)
+    (viz/viz {:DATA [{:x 1 :y 2}
+                     {:x 2 :y 4}
+                     {:x 3 :y 9}]
+              :COLOR "x"
+              :SIZE 200}))
+
+;; Alternatively:
+
+(viz/viz {:viz/type ht/point-chart
+          :DATA [{:x 1 :y 2}
+                 {:x 2 :y 4}
+                 {:x 3 :y 9}]
+          :COLOR "x"
+          :SIZE 200})
+
+;; This last way of using `Viz.clj` can be seen as just a thin convenience on top of calling Hanami directly.
+
+;; ## Datasets
+
+;; Viz.clj defines sensible rendering behaviour for datasets in Clay:
+
+(-> {:x (range 99)
+     :y (repeatedly 99 rand)}
+    tc/dataset)
+
 ;; ## Main principles of plot creation
 
 ;; ### Specifying data sources
@@ -219,7 +341,7 @@
 
 ;; A log transformation would help in linarizing the situation.
 
-;; We can use tablecloth.pipeline to transform data after the start of the viz pipeline, since the internal data representation is compatible with [metamorph](https://github.com/scicloj/metamorph).
+;; We can use tablecloth.pipeline to transform data after the start of the viz pipeline, since the internal data representation is compatible with [Metamorph](https://github.com/scicloj/metamorph).
 
 (-> {:x (range 10)
      :y (map #(Math/pow 10 %)
@@ -230,6 +352,15 @@
     (viz/mark-size 200)
     ((tablecloth.pipeline/update-columns [:y] fun/log10))
     viz/viz)
+
+;; The reason it works is that the main dataset is held under a special key, following the Metamorph approach.
+
+(-> {:x (range 10)
+     :y (map #(Math/pow 10 %)
+             (range 10))}
+    tc/dataset
+    viz/data
+    keys)
 
 ;; ## Using more powerful transofmations
 
@@ -368,14 +499,6 @@
     viz/data
     (viz/type [:histogram {:bin-count 30}])
     viz/viz)
-
-;; ## Datasets
-
-;; Viz.clj defines sensible rendering behaviour for datasets in Clay:
-
-(-> {:x (range 99)
-     :y (repeatedly 99 rand)}
-    tc/dataset)
 
 :bye
 
